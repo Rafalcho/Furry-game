d40e2822c43b983b056f1a11240e239bad69f845
Rafal Cholaj <raf.cholaj@gmail.com>
2017-04-05 19:17:20 +0200

    fixed sounds

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   function Game() {
+
     this.board = document.querySelectorAll('#board > div');
     this.furry = new Furry();
     this.coin = new Coin();
@@ -19,7 +20,9 @@ document.addEventListener('DOMContentLoaded', function() {
     this.index = function(x, y) {
       return x + (y * 10);
     };
+
     this.showFurry = function() {
+
       if (document.querySelector('.furry')) {
         this.hideVisibleFurry();
         this.board[this.index(this.furry.x, this.furry.y)].classList.add('furry');
@@ -28,9 +31,11 @@ document.addEventListener('DOMContentLoaded', function() {
       }
 
     };
+
     this.showCoin = function() {
       this.board[this.index(this.coin.x, this.coin.y)].classList.add('coin');
     };
+
     var self = this;
     this.moveFurry = function() {
 
@@ -43,64 +48,89 @@ document.addEventListener('DOMContentLoaded', function() {
       } else if (this.furry.direction === 'down') {
         this.furry.y = this.furry.y + 1;
       }
+
       this.gameOver();
       this.showFurry();
-      var moveSound = new Audio('../sounds/woosh.wav');
-      moveSound.play();
+
+      document.getElementById('move').play();
+      document.getElementById('move').currentTime = 0;
+
       this.checkCoinCollision();
     };
+
     this.hideVisibleFurry = function() {
       document.querySelector('.furry').classList.remove('furry');
     };
+
     this.startGame = function() {
       this.idSetInterval = setInterval(function() {
         self.moveFurry();
       }, 300);
     };
+
     this.turnFurry = function(event) {
+
       switch (event.which) {
+
         case 37:
           this.furry.direction = 'left';
         break;
+
         case 38:
           this.furry.direction = 'up';
         break;
+
         case 39:
           this.furry.direction = 'right';
         break;
+
         case 40:
           this.furry.direction = 'down';
         break;
 
       }
     };
+
     this.checkCoinCollision = function() {
+
       if (this.furry.x === this.coin.x && this.furry.y === this.coin.y) {
-        var coinSound = new Audio('../sounds/coin.wav');
-        coinSound.play();
+
+        document.getElementById('coin').currentTime = 0;
+        document.getElementById('coin').play();
         document.querySelector('.coin').classList.remove('coin');
+
         this.score += 1;
         document.querySelector('#score strong').textContent = this.score;
         this.coin = new Coin();
         this.showCoin();
       }
     };
+
     this.gameOver = function() {
       if (this.furry.x < 0 || this.furry.x > 9 || this.furry.y < 0 || this.furry.y > 9) {
+
         this.hideVisibleFurry();
         clearInterval(this.idSetInterval);
         this.showFurry = function() {};
-        var overSound = new Audio('../sounds/game_over.flac');
-        overSound.play();
+
+        document.getElementById('game_over').currentTime = 0;
+        document.getElementById('game_over').play();
+
         document.querySelector('#over span').textContent = this.score;
         document.querySelector('#over').classList.remove('invisible');
       }
     };
   }
 
-  var play = document.querySelector('.play');
-  play.addEventListener('click', function() {
+  var playGame = document.querySelector('.play');
+
+  playGame.addEventListener('click', function() {
+
+    document.getElementById('start').currentTime = 0;
+    document.getElementById('start').play();
+
     document.querySelector('#welcome').classList.add('invisible');
+
     var newGame = new Game();
     newGame.showFurry();
     newGame.showCoin();
@@ -112,10 +142,16 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   var playAgain = document.querySelector('.playAgain');
+
   playAgain.addEventListener('click', function() {
+
+    document.getElementById('start').currentTime = 0;
+    document.getElementById('start').play();
+
     document.querySelector('#over').classList.add('invisible');
     document.querySelector('#score strong').textContent = '0';
     document.querySelector('.coin').classList.remove('coin');
+
     newGame = new Game();
     newGame.showFurry();
     newGame.showCoin();
